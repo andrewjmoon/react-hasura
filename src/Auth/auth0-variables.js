4e84ed765a7b37440cd2f7bd72d27b03ed9ef628
Andrew Moon <andrewjm80@gmail.com>
2019-09-11 23:12:57 -0500

    update3

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -2,6 +2,5 @@ import { authDomain, authClientId } from '../constants';
 
 export const AUTH_CONFIG = {
   domain: authDomain,
-  clientId: authClientId,
-  callbackUrl: 'https://centralized-login-eezlzra41.now.sh/callback'
+  clientId: authClientId
 };
